31503fb28c882f9a0982f4276379c40566c8425d
Jean-Luc CANELA <jlcanelam@gmail.com>
2022-01-09 10:49:21 +0100

    clean code

--- build.sc
+++ build.sc
@@ -11,36 +11,16 @@ object sparkapp extends ScalaModule {
 
   import Deps._ 
 
-  val a= 1
   def scalaVersion = "2.12.15"
 
-  /*def repositoriesTask = T.task { super.repositoriesTask() ++ Seq(
-    MavenRepository("https://oss.sonatype.org/content/repositories/snapshots")) 
-  }*/
-//https://oss.sonatype.org/content/repositories/snapshots/dev/zio/zio-prelude/1.0.0-RC8+50-9f687f11-SNAPSHOT/zio-zio-prelude/1.0.0-RC8+50-9f687f11-SNAPSHOT.pom
-//https://oss.sonatype.org/content/repositories/snapshots/dev/zio/zio-prelude_3/1.0.0-RC8+50-9f687f11-SNAPSHOT/zio-prelude_3-1.0.0-RC8+50-9f687f11-SNAPSHOT.pom
   def ivyDeps = Agg(
     ivy"dev.zio::zio::${ZIO_V1}",
     ivy"io.getquill::quill-spark:3.12.0",
     ivy"org.apache.spark::spark-sql:3.2.0",
-  //  ivy"dev.zio::zio-json::0.3.0-RC1-1",
- //   ivy"dev.zio::zio-cache:0.2.0-RC1",
- //   ivy"dev.zio::zio-schema:0.1.5",
-  // Required for automatic generic derivation of schemas
-  //  ivy"dev.zio::zio-schema-derivation:0.1.5"
-//    ivy"org.scala-lang:scala-reflect:${scalaVersion.value},
-    
-    //ivy"dev.zio::zio-prelude:1.0.0-RC6",
-   // ivy"dev:zio::zio-prelude::1.0.0-RC8+50-9f687f11-SNAPSHOT"
-//    https://oss.sonatype.org/content/repositories/snapshots/dev/zio/zio-prelude_3/1.0.0-RC8+50-9f687f11-SNAPSHOT/
-  //  https://oss.sonatype.org/content/repositories/snapshots/dev/zio/zio-prelude_3/1.0.0-RC8+50-9f687f11-SNAPSHOT/
   )
 
-//    def unmanagedClasspath = T {
-//     if (!os.exists(millSourcePath / "lib")) Agg()
-//     else Agg.from(os.list(millSourcePath / "lib").map(PathRef(_)))
-//   }
   override def mainClass = T { Some("App") }
 
+  def forkArgs = Seq("-Dspark.master=local[*]")
 
 }
--- sparkapp/src/App.scala
+++ sparkapp/src/App.scala
@@ -1,14 +1,38 @@
 import org.apache.spark.sql.SparkSession
 
+import io.getquill.QuillSparkContext
 import io.getquill.QuillSparkContext._
 
+case class PersonCount(firstName: String, count: Long)
+
 case class Person(firstName: String, age: Int)
-case class Collaborator(firstName: String, age: Int, company: String)
 
 object App {
+
+  def execute(spark: SparkSession) = {
+    import spark.implicits._
+
+    val persons = liftQuery(
+      spark.createDataset(
+        List(Person("John", 18), Person("Philip", 30), Person("John", 45))
+      )
+    )
+
+    val q = quote {
+      persons.groupBy(_.firstName).map { case (id, cl) =>
+        PersonCount(id, cl.size)
+      }
+    }
+
+    implicit val sqlContext = spark.sqlContext
+    val r = QuillSparkContext.run(q)
+    r.show
+
+  }
+
   def main(args: Array[String]) = {
-    println("hello world !")
-    val sparkSession =
+
+    val spark =
       SparkSession
         .builder()
         .config(
@@ -16,32 +40,10 @@ object App {
           2
         ) // Default shuffle partitions is 200, too much for tests
         .config("spark.ui.enabled", "false")
-        .master("local[1]")
-        .appName("spark test")
+        .appName("spark example")
         .getOrCreate()
 
-    sparkSession.sparkContext.setLogLevel("WARN")
-
-    implicit val sqlContext = sparkSession.sqlContext
-
-    import io.getquill.QuillSparkContext
-    val context = QuillSparkContext
-
-    import sparkSession.implicits._
-
-    val persons = liftQuery(sparkSession.createDataset(List(Person("John", 18), Person("Philip", 30), Person("John", 45))))
-    
-    val q = quote {
-        persons.groupBy(_.firstName).map { case (id, cl) => (id, cl.size)}
-    }
-
-    val q2 = quote {
-        persons.map( p => Collaborator(p.firstName, p.age + 1, "comp"))
-    }
-    
-    val r = QuillSparkContext.run(q2)
-    r.explain(true)
-   // println(r.)
+    execute(spark)
 
   }
 }
